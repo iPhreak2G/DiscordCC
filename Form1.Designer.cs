045594f6916cb677a9f7722cca94353ada845f45
Justin Stolle <iphreak2g@gmail.com>
2019-02-24 22:35:06 -0500

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace DiscordCacheCleaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkVersionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(380, 136);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Options";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Location = new System.Drawing.Point(398, 39);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(144, 136);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Image";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load Cached Images";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(135, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(234, 108);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Delete All Images";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 77);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Delete Selected";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(129, 111);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(550, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.checkVersionToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Version: ";
+            // 
+            // checkVersionToolStripMenuItem
+            // 
+            this.checkVersionToolStripMenuItem.Name = "checkVersionToolStripMenuItem";
+            this.checkVersionToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.checkVersionToolStripMenuItem.Text = "Check Version";
+            this.checkVersionToolStripMenuItem.Click += new System.EventHandler(this.checkVersionToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 184);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Discord Cache Cleaner";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem checkVersionToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace DiscordCacheCleaner
+{
+    public partial class Form1 : Form
+    {
+        public string ver = "discord";
+        public Form1(){
+            InitializeComponent();
+        }
+
+        void Copy(string sourceDir, string targetDir){
+            Directory.CreateDirectory(targetDir);
+
+            foreach(var file in Directory.GetFiles(sourceDir))
+                File.Copy(file, Path.Combine(targetDir, Path.GetFileName(file)));
+
+            foreach(var directory in Directory.GetDirectories(sourceDir))
+                Copy(directory, Path.Combine(targetDir, Path.GetFileName(directory)));
+        }
+
+        void Delete(string targetDir){
+            System.IO.DirectoryInfo di = new DirectoryInfo(targetDir);
+
+            foreach (FileInfo file in di.GetFiles())
+                file.Delete();
+            foreach (DirectoryInfo dir in di.GetDirectories())
+                dir.Delete(true);
+        }
+
+        private void button1_Click(object sender, EventArgs e){
+            try{
+                Delete(@"C:\DiscordCC");
+                string discord = @"\discord";
+                string userName = Environment.UserName;
+                string newdir;
+                string olddir;
+                if (userName.Contains("-PC")) userName.Replace("-PC", "");
+                if (Directory.Exists(@"C:\Users\" + userName + @"\AppData\Roaming\discord") && Directory.Exists(@"C:\Users\" + userName + @"\AppData\Roaming\discordptb"))
+                {
+                    DialogResult res = MessageBox.Show("You Have Both DiscordPTB & Discord Installed Please Select Yes for Discord or No for DiscordPTB", "Multiple Discord Clients Found", MessageBoxButtons.YesNo);
+                    if (res == DialogResult.Yes)
+                    {
+                        label1.Text = "Version: Discord";
+                        ver = "discord";
+                        discord = @"\discord";
+                        newdir = @"C:\DiscordCC";
+                        olddir = @"C:\Users\" + userName + @"\AppData\Roaming" + discord + @"\Cache";
+                        Directory.CreateDirectory(newdir);
+                        Copy(olddir, newdir);
+                        DirectoryInfo d = new DirectoryInfo(newdir);
+                        FileInfo[] Files = d.GetFiles("*");
+                        foreach (FileInfo file in Files)
+                        {
+                            string f = file.Name + ".png";
+                            File.Move(d + @"\" + file.Name, d + @"\" + f);
+                            listBox1.Items.Add(d + @"\" + f);
+                        }
+                    }
+                    else if (res == DialogResult.No)
+                    {
+                        label1.Text = "Version: DiscordPTB";
+                        ver = "discordptb";
+                        discord = @"\discordptb";
+                        newdir = @"C:\DiscordCC";
+                        olddir = @"C:\Users\" + userName + @"\AppData\Roaming" + discord + @"\Cache";
+                        Directory.CreateDirectory(newdir);
+                        Copy(olddir, newdir);
+                        DirectoryInfo d3 = new DirectoryInfo(newdir);
+                        FileInfo[] Files3 = d3.GetFiles("*");
+                        foreach (FileInfo file in Files3)
+                        {
+                            string f = file.Name + ".png";
+                            File.Move(d3 + @"\" + file.Name, d3 + @"\" + f);
+                            listBox1.Items.Add(newdir + @"\" + f);
+                        }
+                    }
+                }
+                else
+                {
+                    if (Directory.Exists(@"C:\Users\" + userName + @"\AppData\Roaming\discord"))
+                    {
+                        label1.Text = "Version: Discord";
+                        ver = "discord";
+                        discord = @"\discord";
+                    }
+                    if (Directory.Exists(@"C:\Users\" + userName + @"\AppData\Roaming\discord"))
+                    {
+                        label1.Text = "Version: DiscordPTB";
+                        ver = "discordptb";
+                        discord = @"\discordptb";
+                    }
+                    newdir = @"C:\DiscordCC";
+                    olddir = @"C:\Users\" + userName + @"\AppData\Roaming" + discord + @"\Cache";
+                    Directory.CreateDirectory(newdir);
+                    Copy(olddir, newdir);
+                    DirectoryInfo d2 = new DirectoryInfo(newdir);
+                    FileInfo[] Files2 = d2.GetFiles("*");
+                    foreach (FileInfo file in Files2)
+                    {
+                        string f = file.Name + ".png";
+                        File.Move(d2 + @"\" + file.Name, d2 + @"\" + f);
+                        listBox1.Items.Add(d2 + @"\" + f);
+                    }
+                }
+            }catch(Exception ex){
+                var st = new StackTrace(ex, true);
+                var frame = st.GetFrame(0);
+                var line = frame.GetFileLineNumber();
+                MessageBox.Show("Error: "+ex.Message, "An Error Occurred: "+line+"!");
+                Application.Exit();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string userName = Environment.UserName;
+                Delete(@"C:\DiscordCC");
+                Delete(@"C:\Users\" + userName + @"\AppData\Roaming\" + ver + @"\Cache");
+                System.Diagnostics.Process.Start("cmd.exe", "/c taskkill /IM "+ver+".exe /T /F");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "An Error Has Occurred!");
+                Application.Exit();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try{
+                //MessageBox.Show(listBox1.SelectedItem.ToString());
+                File.Delete(listBox1.SelectedItem.ToString());
+                listBox1.Items.Remove(listBox1.SelectedItem);
+            }catch (Exception ex){
+                MessageBox.Show("Error: " + ex.Message, "An Error Has Occurred!");
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                pictureBox1.ImageLocation = listBox1.SelectedItem.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "An Error Has Occurred!");
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Created By: Justin Stolle (Cyanokit)\nSpecial Thanks to Tex for the idea!\n\nBuild 1.0.0.0\nDate: 2/24/2019");
+        }
+
+        private void checkVersionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string userName = Environment.UserName;
+            if (Directory.Exists(@"C:\Users\" + userName + @"\AppData\Roaming\discord") && Directory.Exists(@"C:\Users\" + userName + @"\AppData\Roaming\discord"))
+            {
+                MessageBox.Show("You have both version of discord!");
+                ver = "Version: Both";
+            }
+            else if (Directory.Exists(@"C:\Users\" + userName + @"\AppData\Roaming\discord"))
+            {
+                MessageBox.Show("You have the regular versions of discord installed!");
+                ver = "Version: Discord";
+            }
+            else if (Directory.Exists(@"C:\Users\" + userName + @"\AppData\Roaming\discordptb"))
+            {
+                MessageBox.Show("You have DiscordPTB installed!");
+                ver = "Version: DiscordPTB";
+            }
+            else
+            {
+                MessageBox.Show("You do not have discord installed!");
+                ver = "Version: N/A";
+            }
+        }
+    }
+}
